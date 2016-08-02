d193bf6d404c28a4a1c523ae175c7f12d9daae39
Plata <rodrigo.s.plata@accenture.com>
2016-08-02 18:24:24 -0300

    restruturacao para evolucao do ambiente base

--- 99-Base-para-novos-cases/nodeJS/gethApp.js
+++ 99-Base-para-novos-cases/nodeJS/gethApp.js
@@ -19,33 +19,133 @@ const gestao_garantiasAddress = "0xbbbc8275f9f355211020a52957246cd763e48484";
 const PORT = 9090;
 
 
-var app = require('express')();
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var session = require('express-session');
+app.use(session({secret: 'AccentureCaduePlata'}));
+
 var server = require('http').Server(app);
-//var io = require('socket.io')(server});
-var io = require('socket.io')(server, {v: 'socket.io'});
+var sio = require('socket.io')(server);
+var io = null;
+const usernames = {};
+var redis   = require("redis");
+var client  = redis.createClient();
+
+client.on('connect', function() {
+    console.log('redis connected');
+});
 
 var socketObj;
 
+server.listen(PORT, "localhost");
+//io = sio.listen(server)
+console.log('Running on http://localhost:' + PORT);
+//console.log('Running on http://ec2-54-171-185-229.eu-west-1.compute.amazonaws.com:' + PORT);
+//console.log('Running on http://ec2-54-171-185-229.eu-west-1.compute.amazonaws.com/node');
+
+app.get('/', function (req, res) {
+    //req.sessionID //Id do usuário que deve ser usado para transmissão de manssagem privadas
+    //req.headers['x-forwarded-for'] //Id do usuário que deve ser usado para transmissão de manssagem privadas, em conjunto com o sessionID
+    //res.sendFile(__dirname + '/client.html');
+
+    //debugger
+
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    res.render('index', {
+      sessionID: req.sessionID,
+      ip: ip
+    });
+    //console.log('sessionID: ' + req.sessionID);
+    //console.log('ip: ' + req.headers['x-forwarded-for']);
+});
+
+sio.sockets.on('connection', (socket) => {
+  socket.on('sendchat', (data) => {
+    sio.sockets.emit('updatechat', socket.username, data);
+  });
+
+  socket.on('adduser', (sessionID) => {
+    socket.sessionID = sessionID;
+
+    //usernames[username] = username;
+
+    socket.emit(
+      'servernotification', {
+        connected: true,
+        toSelf: true,
+        sessionID: sessionID
+      });
+
+    //socket.broadcast.emit('servernotification', { connected: true, sessionID: sessionID });
+
+    //sio.sockets.emit('updateusers', usernames);
+
+    console.log('adduser: ' + sessionID);
+  });
+
+  socket.on('disconnect', () => {
+    delete usernames[socket.username];
+
+    sio.sockets.emit('updateusers', usernames);
+
+    socket.broadcast.emit('servernotification', { username: socket.username });
+  });
+});
+
+function addUser(sessionID, ip, userName) {
+    if (client.sadd('sessoes', sessionID) == 1) {
+        client.hmset(sessionID, 'ip', ip, 'userName', userName)
+    } else {
+        var ip_aux = client.hmget(sessionID, 'ip');
+        var uerName_aux = client.hmget(sessionID, 'userName');
+        
+        if (ip_aux != ip || uerName_aux != uerName) {      
+            console.log("desconectar usuario");
+        }
+    }
+}
+
+
+
+
 // creating a new websocket to keep the content updated without any AJAX request
-
-
-io.sockets.on('connection', function(socket) {
+/*
+sio.sockets.on('connection', function(socket) {
 
   console.log(__dirname);
 
   socketObj = socket;
 
 });
+*/
 
+/*
+io.use(function () {
+  function auth (data, fn) {
+    var cookies = parse_cookies(data.headers.cookie);
+    console.log('PHPSESSID: ' + cookies.PHPSESSID);
 
+        client.sismember('sid', cookies.PHPSESSID, function (err , reply) {
+            fn(null, reply);    
+        });
+  };
+
+  io.set('authorization', auth);
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('access', 'granted');
+});
+*/
 
 // App
-app.get('/', function (req, res) {
-    req.sessionID //Id do usuário que deve ser usado para transmissão de manssagem privadas
-    req.headers['x-forwarded-for'] //Id do usuário que deve ser usado para transmissão de manssagem privadas, em conjunto com o sessionID
-    res.sendFile(__dirname + '/client.html');
-    console.log('pagina 1');
-});
 
 
 app.post('/consultar', urlencodedParser, function (req, res) {
@@ -101,8 +201,13 @@ app.get('/send_transaction', function (req, res) {
 
 
 
-//app.listen(PORT);
-server.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
-console.log('Running on http://ec2-54-171-185-229.eu-west-1.compute.amazonaws.com:' + PORT);
-console.log('Running on http://ec2-54-171-185-229.eu-west-1.compute.amazonaws.com/node');
+function parse_cookies(_cookies) {
+    var cookies = {};
+
+    _cookies && _cookies.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        cookies[ parts[ 0 ].trim() ] = ( parts[ 1 ] || '' ).trim();
+    });
+
+    return cookies;
+}
